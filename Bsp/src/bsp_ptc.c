f350e74e0f774bc36e7b08122522b7d6628c373b
haliwt <jonewang@126.com>
2025-01-13 11:35:20 +0800

    update adjust set temperature value that fan moment stop is bug ,by solved.

--- Bsp/src/bsp_ptc.c
+++ Bsp/src/bsp_ptc.c
@@ -22,10 +22,21 @@ void Ptc_Off(void)
 void set_ptc_value_conifrm_handler(uint8_t value)
 {
 
-   lcd_donot_temp_value_fun();
-   osDelay(500);
+  lcd_donot_temp_value_fun();
+   //osDelay(500);
+   osDelay(100);
+   fan_leaf_icon_run_handler();
+   osDelay(100);
+   fan_leaf_icon_run_handler();
+   osDelay(100);
+   fan_leaf_icon_run_handler();
+   osDelay(100);
+   fan_leaf_icon_run_handler();
+   osDelay(100);
+   fan_leaf_icon_run_handler();
 
   lcd_disp_ptc_value(value);
+  fan_leaf_icon_run_handler();
 }
 
 
