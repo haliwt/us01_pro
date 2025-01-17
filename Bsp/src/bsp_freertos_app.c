38af54280de15c7a950e4b9a79d5e7fc7bc61c2d
haliwt <jonewang@126.com>
2025-01-17 10:50:31 +0800

    update

--- Bsp/src/bsp_freertos_app.c
+++ Bsp/src/bsp_freertos_app.c
@@ -328,29 +328,37 @@ static void vTaskStart(void *pvParameters)
         }
       }
       else if(KEY_ADD_VALUE() == KEY_DOWN){
-//         xTaskNotify(xHandleTaskMsgPro,  /* 目标任务 */
-//                ADD_KEY_3,     /* 设置目标任务事件标志位bit0  */
-//                eSetBits);  /* 将目标任务的事件标志位与BIT_0进行或操作， 将结果赋值给事件标志位 */
-          power_key_long_conter=0;
+        #if 0
+         xTaskNotify(xHandleTaskMsgPro,  /* 目标任务 */
+                ADD_KEY_3,     /* 设置目标任务事件标志位bit0  */
+                eSetBits);  /* 将目标任务的事件标志位与BIT_0进行或操作， 将结果赋值给事件标志位 */
+          
+         #else 
+           power_key_long_conter=0;
           gpro_t.long_key_mode_counter=0;
-
           if(gkey_t.key_power==power_on){
              gpro_t.key_add_flag = 1;
 
            }
+          #endif 
                
      }
      else if(KEY_DEC_VALUE() == KEY_DOWN){
-//        xTaskNotify(xHandleTaskMsgPro,  /* 目标任务 */
-//                DEC_KEY_2,     /* 设置目标任务事件标志位bit0  */
-//                eSetBits);  /* 将目标任务的事件标志位与BIT_0进行或操作， 将结果赋值给事件标志位 */
-           power_key_long_conter=0;
+
+     #if 0
+        xTaskNotify(xHandleTaskMsgPro,  /* 目标任务 */
+                DEC_KEY_2,     /* 设置目标任务事件标志位bit0  */
+                eSetBits);  /* 将目标任务的事件标志位与BIT_0进行或操作， 将结果赋值给事件标志位 */
+         
+      #else
+         power_key_long_conter=0;
           gpro_t.long_key_mode_counter=0;
          if(gkey_t.key_power==power_on){
-          gpro_t.key_dec_flag = 1; 
-          }
+            gpro_t.key_dec_flag = 1; 
+         }
   
      }
+     #endif 
         
      vTaskDelay(20);
     }
